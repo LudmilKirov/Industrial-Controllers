d0514102362d547bde0df092b0406eb77b10031e
LudmilKirov <45374371+LudmilKirov@users.noreply.github.com>
2019-03-16 16:08:11 +0200

    v0.1 pre-Alpha

---
+++ user.c
@@ -0,0 +1,43 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+/* Device header file */
+#if defined(__XC16__)
+    #include <xc.h>
+#elif defined(__C30__)
+    #if defined(__dsPIC33E__)
+    	#include <p33Exxxx.h>
+    #elif defined(__dsPIC33F__)
+    	#include <p33Fxxxx.h>
+    #endif
+#endif
+
+#include <stdint.h>          /* For uint16_t definition                       */
+#include <stdbool.h>         /* For true/false definition                     */
+#include "user.h"            /* variables/params used by user.c               */
+
+/******************************************************************************/
+/* User Functions                                                             */
+/******************************************************************************/
+
+/* <Initialize variables in user.h and insert code for user algorithms.> */
+
+void InitApp(void)
+{
+    /* TODO Initialize User Ports/Peripherals/Project here */
+
+    /* Setup analog functionality and port direction */
+
+    /* Initialize peripherals */
+TRISBbits.TRISB13=0;//napravo portb pin13 izhod
+INITPWM();
+}
+
+void INITPWM(){
+    P1TPERbits.PTPER=2000;//
+    P1DC1=1000;
+    P1DC2=1000;
+    P1DC3=1000;
+    P1TCONbits.PTEN=1;
+}
